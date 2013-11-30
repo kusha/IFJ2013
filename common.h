dda2edd75437ca85e6560e812bac9025502cb43b
Birger Mark <kusha@Mac-Pro-Mark.local>
2013-11-30 02:06:20 +0100

    cosmetic update + colors in common

--- common.h
+++ common.h
@@ -1,21 +1,47 @@
+/* -- IFJ project 2013 ------------------------------------------------------
+**
+**	Interpreter of IFJ2013 language
+**	4.11.2013 - 15.12.2013
+**
+**	Team 13 (b/3/I):
+**
+**	Bank Tomáš			<xbankt00@stud.fit.vutbr.cz>
+**	Birger Mark			<xbirge00@stud.fit.vutbr.cz>
+**	Botka Roland		<xbotka00@stud.fit.vutbr.cz>
+**	Brandejs Zdenko		<xbrand06@stud.fit.vutbr.cz>
+**	Khudiakov Daniil	<xkhudi00@stud.fit.vutbr.cz>
+**
+**	Defines for all units of the project.
+**
+** -------------------------------------------------------------------------*/
+
+
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG_FLAG		1
 
-//exit codes:
-#define SUCCESS 		0
-#define LEXICAL_ERROR 	1
-#define PARSER_ERROR 	2
-//semantic errors
-#define S_FUNC_ERROR 	3
-#define S_PARAM_ERROR 	4
-#define S_DECLAR_ERROR 	5
-#define S_ZERODIV_ERROR	10
-#define S_TYPE_ERROR 	11
-#define S_EXPRESS_ERROR	12
-#define S_OTHER_ERROR 	13
-//interpret error
-#define INTERNAL_ERROR 	99
+#define DEBUG_FLAG		1 	// print debug output flag
+
+#define CNRM  "\x1B[0m"		// print color code: normal
+#define CRED  "\x1B[31m"	// print color code: red
+#define CGRN  "\x1B[32m"	// print color code: green
+#define CYEL  "\x1B[33m"	// print color code: yellow
+#define CBLU  "\x1B[34m"	// print color code: blue
+#define CMAG  "\x1B[35m"	// print color code: magenta
+
+#define SUCCESS 		0 	// exit codes: successfull operation
+#define LEXICAL_ERROR 	1	// exit codes: lexical analysis error
+#define PARSER_ERROR 	2	// exit codes: syntax analysis error
+
+#define S_FUNC_ERROR 	3	// exit codes: semantic error: un/re def function
+#define S_PARAM_ERROR 	4	// exit codes: semantic error: func params error
+#define S_DECLAR_ERROR 	5	// exit codes: semantic error: undeclared var
+#define S_ZERODIV_ERROR	10	// exit codes: semantic error: divide by zero
+#define S_TYPE_ERROR 	11	// exit codes: semantic error: type conversion
+#define S_EXPRESS_ERROR	12	// exit codes: semantic error: expression comptab
+#define S_OTHER_ERROR 	13	// exit codes: semantic error: another case
+
+#define INTERNAL_ERROR 	99	// exit codes: internal interpreter error
+
 
 #endif
--- main.c
+++ main.c
@@ -21,15 +21,6 @@
 	listDispose(&instructionList); \
 	fclose(file);
 
-// DEBUG COLORS!!! TO DELETE
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-
-
 int main (int argc, char*argv[]) {
 
 	//input data section
